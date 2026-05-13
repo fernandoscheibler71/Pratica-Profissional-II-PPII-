c99a8a54a34f2457201658987e5389ca994bfdc6
Davi <davicostta54@gmail.com>
2026-05-13 20:08:34 -0300

    add CORS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.json())
+const cors = require ('cors')
+
+app.use(cors({
+  origin: "https://nextweb-front.vercel.app/"
+}));
 
 const user = require('./routes/RouteUser')
 const community = require('./routes/RouteCommunity')
 const communityUser = require('./routes/RouteCommunityUser')
+
 
 app.use('/user', user)
 app.use('/community', community)
