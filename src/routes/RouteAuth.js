3faf2b852e7d075077fb337426699ac7d87392cc
Davi <davicostta54@gmail.com>
2026-06-17 22:47:00 -0300

    corrigindo erro no Auth

--- src/routes/RouteAuth.js
+++ src/routes/RouteAuth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const AuthController = require('../controllers/AuthController')
+const AuthController = require('../controllers/authController')
 
 const auth = new AuthController()
 
